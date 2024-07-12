e8b603123b35b0770f3b7f7ca3c14fb917235679
Pavan Prasad <110252432+Pavanmum@users.noreply.github.com>
2024-07-12 23:10:46 +0530

    feat: add path module for file handling

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ import errorMiddleware from "./middleware/error.js";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 
